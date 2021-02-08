458b475b0317579c57f8019315a584c17cd4417e
nwainwri <nathanwainwright@gmail.com>
2021-02-07 21:04:41 -0800

    tableView works, and custom tableViewCell added

---
+++ delegatePractice/ThirtyDayCell.swift
@@ -0,0 +1,19 @@
+//
+//  ThirtyDayCell.swift
+//  delegatePractice
+//
+//  Created by Nathan Wainwright on 2021-02-07.
+//
+
+import UIKit
+
+class ThirtyDayCell: UITableViewCell {
+
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
--- delegatePractice/ViewController.swift
+++ delegatePractice/ViewController.swift
@@ -7,12 +7,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+  let tableView: UITableView = {
+    let tv = UITableView()
+    tv.backgroundColor = UIColor.red
+    tv.translatesAutoresizingMaskIntoConstraints = false
+    return tv
+  }()
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    setupTableView()
+
   }
+
+  func setupTableView() {
+    tableView.delegate = self
+    tableView.dataSource = self
+    tableView.register(ThirtyDayCell.self, forCellReuseIdentifier: "cellId")
+    view.addSubview(tableView)
+
+    NSLayoutConstraint.activate([
+      tableView.topAnchor.constraint(equalTo: self.view.topAnchor),
+      tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+      tableView.rightAnchor.constraint(equalTo: self.view.rightAnchor),
+      tableView.leftAnchor.constraint(equalTo: self.view.leftAnchor)
+    ])
+  }
+
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    //1
+    return 100
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    //2
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath) as! ThirtyDayCell
+    cell.backgroundColor = UIColor.blue
+    return cell
+  }
+
+
+
+
+
 
 
 }
